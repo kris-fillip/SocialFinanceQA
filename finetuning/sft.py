5e67ca28d51b1e1314279a99252c839a69154ee3
kris-fillip <kahl.krisfillip98@gmail.com>
2024-10-04 16:02:50 +0200

    remove extracted functions

--- finetuning/sft.py
+++ finetuning/sft.py
@@ -204,36 +204,6 @@ def fine_tune(model_name: str, train_data: Dataset, validation_data: Dataset) ->
     reloaded_tokenizer.save_pretrained(merged_dir)
 
 
-def remove_unused_columns(data: Dataset) -> Dataset:
-    all_columns = data.column_names
-    columns_to_keep = ["text", "prompt", "chosen", "rejected"]
-    for col in columns_to_keep:
-        if col in all_columns:
-            all_columns.remove(col)
-    data = data.remove_columns(column_names=all_columns)
-    return data
-
-
-def preprocess_sft(example: dict[any]) -> dict[any]:
-    example["text"] = f"[INST] {example['text']} {example['context']} [/INST] {example['answer_1']}"
-    return example
-
-def preprocess_sft_zephyr(example: dict[any]) -> dict[any]:
-    example["text"] = f"<|system|>\n</s>\n<|user|>\n{example['text']} {example['context']}</s>\n<|assistant|>\n{example['answer_1']}</s>\n"
-    return example
-
-def preprocess_dpo(example: dict[any]) -> dict[any]:
-    example["prompt"] = f"[INST] {example['text']} {example['context']} [/INST]"
-    example["chosen"] = example['answer_1']
-    example["rejected"] = example['answer_2']
-    return example
-
-def preprocess_dpo_zephyr(example: dict[any]) -> dict[any]:
-    example["prompt"] = f"<|system|>\n</s>\n<|user|>\n{example['text']} {example['context']}</s>\n",
-    example["chosen"] = f"<|assistant|>\n{example['answer_1']}</s>\n",
-    example["rejected"] = f"<|assistant|>\n{example['answer_2']}</s>\n",
-    return example
-
 def sft(model_name: str, validation_size: int) -> None:
     data = load_dataset("Kris-Fillip/SocialFinanceQA", split="train")
     if "zephyr" in model_name:
