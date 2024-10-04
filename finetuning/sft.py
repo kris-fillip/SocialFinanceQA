afef004b647742b282815721c8ad09991ef07bcf
kris-fillip <kahl.krisfillip98@gmail.com>
2024-10-04 15:19:47 +0200

    update sft.py

--- finetuning/sft.py
+++ finetuning/sft.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import torch
 from transformers import (
@@ -10,10 +11,8 @@ from datasets import load_dataset, Dataset
 from peft import LoraConfig, AutoPeftModelForCausalLM
 from trl import SFTTrainer
 import transformers
-import json
 from huggingface_hub import login
 from tqdm import tqdm
-from huggingface_hub import HfApi
 
 def chars_token_ratio(dataset, tokenizer, nb_examples=400):
     """
@@ -29,11 +28,9 @@ def chars_token_ratio(dataset, tokenizer, nb_examples=400):
 
     return total_characters / total_tokens
 
-def fine_tune(train_data, validation_data):
+def fine_tune(model_name, train_data, validation_data):
     print(f"using {len(train_data)} train samples and {len(validation_data)} validation samples")
     login()
-    # Set the name of the model we'll use for the rest of the notebook
-    model_name = "mistralai/Mistral-7B-v0.1"
     device_count = torch.cuda.device_count()
     for i in range(device_count):
         print(f"Device {i}: {torch.cuda.get_device_name(i)}")
@@ -51,8 +48,7 @@ def fine_tune(train_data, validation_data):
 
     # Activate nested quantization for 4-bit base models (double quantization)
     use_nested_quant = False
-    # Load dataset (you can process it here)
-    # dataset = load_dataset(path=os.path.join(data_path, "final_complete_qa.json"), split="train")
+
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=use_4bit,
         bnb_4bit_quant_type=bnb_4bit_quant_type,
@@ -85,8 +81,6 @@ def fine_tune(train_data, validation_data):
     
     chars_per_token = chars_token_ratio(train_data, tokenizer)
     print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
-
-    new_model = 'Llama-7b-reddit-finance'
 
     # LoRA attention dimension
     lora_r = 16
@@ -159,7 +153,6 @@ def fine_tune(train_data, validation_data):
         ddp_find_unused_parameters=False,
         save_total_limit=5,
         load_best_model_at_end=True,
-        # auto_find_batch_size=True,
         gradient_checkpointing_kwargs={
             "use_reentrant": False
         },
@@ -179,15 +172,9 @@ def fine_tune(train_data, validation_data):
         args=training_arguments,
         packing=packing,
         max_seq_length=max_seq_length,
-        # num_of_sequences=1,
         chars_per_token=chars_per_token,
-        # neftune_noise_alpha=10.0
     )
-    cur_tokenized = trainer.train_dataset.to_dict()
 
-    # with open("tokenized_train_data_zephyr_unpacked_correct.json", "w") as out_file:
-    #     json.dump(cur_tokenized, out_file)
-    # return
     transformers.logging.set_verbosity_info()
     trainer.train()
 
@@ -212,17 +199,50 @@ def fine_tune(train_data, validation_data):
     merged_model.save_pretrained(merged_dir)
     reloaded_tokenizer.save_pretrained(merged_dir)
 
-    api = HfApi(token="token")
 
-    api.create_repo(f"Kris-Fillip/{output_dir}", repo_type="model", token="token")
-    merged_model.push_to_hub("Kris-Fillip/{output_dir}", private=True)
-    reloaded_tokenizer.push_to_hub("Kris-Fillip/{output_dir}", private=True)
+def remove_unused_columns(data: Dataset):
+    all_columns = data.column_names
+    columns_to_keep = ["text", "prompt", "chosen", "rejected"]
+    for col in columns_to_keep:
+        if col in all_columns:
+            all_columns.remove(col)
+    data = data.remove_columns(column_names=all_columns)
+    return data
+
+
+def preprocess_sft(example):
+    example["text"] = f"[INST] {example['text']} {example['context']} [/INST] {example['answer_1']}"
+    return example
+
+def preprocess_sft_zephyr(example):
+    example["text"] = f"<|system|>\n</s>\n<|user|>\n{example['text']} {example['context']}</s>\n<|assistant|>\n{example['answer_1']}</s>\n"
+    return example
+
+def preprocess_dpo(example):
+    example["prompt"] = f"[INST] {example['text']} {example['context']} [/INST]"
+    example["chosen"] = example['answer_1']
+    example["rejected"] = example['answer_2']
+    return example
+
+def preprocess_dpo_zephyr(example):
+    example["prompt"] = f"<|system|>\n</s>\n<|user|>\n{example['text']} {example['context']}</s>\n",
+    example["chosen"] = f"<|assistant|>\n{example['answer_1']}</s>\n",
+    example["rejected"] = f"<|assistant|>\n{example['answer_2']}</s>\n",
+    return example
+
+def sft(model_name: str) -> None:
+    data = load_dataset("Kris-Fillip/SocialFinanceQA", split="train")
+    updated_dataset = data.map(preprocess_sft)
+    updated_dataset = remove_unused_columns(updated_dataset)
+    train_validation = updated_dataset.train_test_split(test_size=1000, shuffle=False)
+    train = train_validation["train"]
+    validation = train_validation["test"]
+    fine_tune(model_name, train, validation)
 
 
 if __name__ == "__main__":
-    data_path = "/data"
-    with open(os.path.join(data_path, "sft_train_tokenized_filter.json"), "r") as data_file:
-        train = json.load(data_file)
-    with open(os.path.join(data_path, "sft_validation_tokenized_filter.json"), "r") as data_file:
-        validation = json.load(data_file)
-    fine_tune(Dataset.from_list(train), Dataset.from_list(validation))
+    parser = argparse.ArgumentParser("sft")
+    parser.add_argument("-m", "--model_name", help="Model name to use as base model for the finetuning",
+                        type=str, default="meta-llama/Llama-2-7b-hf")
+    args = parser.parse_args()
+    sft(model_name=args.model_name)
